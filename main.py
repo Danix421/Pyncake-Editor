b9add36511e936c0d226ab97a596fd8c791ea329
Danix <danix@disroot.org>
2021-03-15 14:39:35 -0400

    Editor's preview version complete

---
+++ main.py
@@ -0,0 +1,57 @@
+# Pyncake Editor
+# Made by Danix
+# Font "Silver" by Poppy Works
+
+import pygame, sys
+from pygame.locals import *
+from Data.colors import Colors
+
+pygame.init()
+
+class app:
+    class metadata:
+        screen = pygame.display.Info()
+        app_name = "Pyncake Editor"
+        app_icon = pygame.image.load("app_icon.png")
+        app_logo = pygame.image.load("Data/Graphics/pyncake.png")
+        window_size = (950, 750)
+    
+    class data:
+        data_dir = "Data/"
+        graphics_dir = data_dir + "Graphics/"
+        fonts_dir = data_dir + "Fonts/"
+        font = pygame.font.Font(fonts_dir + "Silver.ttf", 30)
+        colors = Colors()
+        exit = font.render("X", True, colors.white)
+    
+    def __init__(self):
+        self.window = pygame.display.set_mode(self.metadata.window_size, pygame.NOFRAME)
+        pygame.display.set_caption(self.metadata.app_name)
+        pygame.display.set_icon(self.metadata.app_icon)
+        self.running = True
+
+        self.data.exitbutton = pygame.draw.rect(self.window, self.data.colors.grey, (880, 0, 70, 25))
+        self.window.blit(self.data.exit, (912, 1))
+        self.data.down_bar = pygame.draw.rect(self.window, self.data.colors.pancake, (0, 730, 950, 20))
+        self.data.left_bar = pygame.draw.rect(self.window, self.data.colors.blue, (0, 0, 45, 750))
+        self.data.up_bar = pygame.draw.rect(self.window, self.data.colors.grey, (0, 0, 1000, 25))
+
+        self.window.blit(self.metadata.app_logo, (150, 150))
+
+        while self.running:
+            self.data.mouse_pos = pygame.mouse.get_pos()
+            if self.data.exitbutton.collidepoint(self.data.mouse_pos):
+                self.data.exitbutton = pygame.draw.rect(self.window, self.data.colors.red, (880, 0, 70, 25))
+                self.window.blit(self.data.exit, (912, 1))
+            else:
+                self.data.exitbutton = pygame.draw.rect(self.window, self.data.colors.grey, (880, 0, 70, 25))
+                self.window.blit(self.data.exit, (912, 1))
+            for self.event in pygame.event.get():
+                if self.event.type == MOUSEBUTTONDOWN:
+                    if self.event.button == 1:
+                        if self.data.exitbutton.collidepoint(self.data.mouse_pos):
+                            pygame.quit()
+                            sys.exit()
+            pygame.display.update()
+
+app()

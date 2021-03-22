f387e938b8fb926c5360603d9995092396f57be7
Danix <78866931+Danix421@users.noreply.github.com>
2021-03-22 20:38:21 -0300

    Added Discord Rich Presence and update config file

--- main.py
+++ main.py
@@ -27,27 +27,38 @@ class app:
         exit = font.render("X", True, colors.white)
         
         data = {
-           "Packages" : "Script"
+           "rich presence": True
         }
         
-        with open('project.json', 'w+') as project_file:
-            json.dump(data, project_file)
-        
+        try:
+            config = json.load(open("Data/config.json", "r"))
+        except FileNotFoundError:
+            json.dump(data, open("Data/config.json", "w"))
+            config = open("Data/config.json", "r")
+    
     def __init__(self):
         self.window = pygame.display.set_mode(self.metadata.window_size, pygame.NOFRAME)
         pygame.display.set_caption(self.metadata.app_name)
         pygame.display.set_icon(self.metadata.app_icon)
         self.running = True
-
-        self.data.exitbutton = pygame.draw.rect(self.window, self.data.colors.grey, (880, 0, 70, 25))
+        
+        if self.data.config["rich presence"]:
+            try:
+                self.RP = pypresence.Presence("811677670718570536")
+                self.RP.connect()
+                self.RP.update("large_icon" = "appicon", "large_text" = "Pyncake Editor", "state" = "In editor")
+            except pypresence.exceptions.InvalidPipe:
+                pass
+        
         pygame.mouse.set_visible(False)
         while self.running:
             self.window.fill((0,0,0))
             self.data.mouse_pos = pygame.mouse.get_pos()
             self.window.blit(self.data.exit, (912, 1))
-            self.data.down_bar = pygame.draw.rect(self.window, self.data.colors.pancake, (0, 730, 950, 20))
-            self.data.left_bar = pygame.draw.rect(self.window, self.data.colors.blue, (0, 0, 45, 750))
-            self.data.up_bar = pygame.draw.rect(self.window, self.data.colors.grey, (0, 0, 1000, 25))
+            self.down_bar = pygame.draw.rect(self.window, self.data.colors.pancake, (0, 730, 950, 20))
+            self.left_bar = pygame.draw.rect(self.window, self.data.colors.blue, (0, 0, 45, 750))
+            self.up_bar = pygame.draw.rect(self.window, self.data.colors.grey, (0, 0, 1000, 25))
+            self.exitbutton = pygame.draw.rect(self.window, self.data.colors.grey, (880, 0, 70, 25))
             self.window.blit(self.metadata.app_logo, (150, 150))
             if self.data.exitbutton.collidepoint(self.data.mouse_pos):
                 self.data.exitbutton = pygame.draw.rect(self.window, self.data.colors.red, (880, 0, 70, 25))
@@ -59,7 +70,7 @@ class app:
             for self.event in pygame.event.get():
                 if self.event.type == MOUSEBUTTONDOWN:
                     if self.event.button == 1:
-                        if self.data.exitbutton.collidepoint(self.data.mouse_pos):
+                        if self.exitbutton.collidepoint(self.data.mouse_pos):
                             pygame.quit()
                             sys.exit()
 
